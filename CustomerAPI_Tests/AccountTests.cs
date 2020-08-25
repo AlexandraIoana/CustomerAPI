812beb8146c927002ec820339d5250245f25b3ef
alexandra <alexandra.pletea@outlook.com>
2020-08-25 17:14:26 +0100

    Created and ran unit tests for POST endpoint for account

--- CustomerAPI_Tests/AccountTests.cs
+++ CustomerAPI_Tests/AccountTests.cs
@@ -1,5 +1,8 @@
-﻿using CustomerAPI.Data.Models;
+﻿using AutoMapper;
+using CustomerAPI.Controllers;
+using CustomerAPI.Data.Models;
 using CustomerAPI.Interfaces;
+using CustomerAPI.Resources.Mapping;
 using Moq;
 using NUnit.Framework;
 using System;
@@ -7,14 +10,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CustomerAPI.Resources.Extensions;
+using CustomerAPI.Resources.Communication;
+using CustomerAPI.Resources.Classes;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CustomerAPI_Tests
 {
     [TestFixture]
     public class AccountTests
     {
+        AccountController _accountController;
         Mock<IAccountService> _accountServiceMock;
         List<Account> accounts;
+        Account accountToTestSave;
+        SaveAccountResource saveAccountResource;
+        Mapper _mapper;
 
         [SetUp]
         public void Setup()
@@ -27,11 +38,33 @@ namespace CustomerAPI_Tests
                 new Account { ID = 4, Balance = 0, Transactions = new List<Transaction>(), CustomerID = 1}
             };
 
+            saveAccountResource = new SaveAccountResource()
+            {
+                CustomerID = 1,
+                InitialCredit = 21
+            };
+
+            accountToTestSave = new Account()
+            {
+                ID = 0,
+                Balance = 21,
+                CustomerID = 1
+            };
+
             _accountServiceMock = new Mock<IAccountService>();
             _accountServiceMock.Setup(a => a.GetAccountsForCustomerAsync(1)).ReturnsAsync(accounts);
+            _accountServiceMock.Setup(a => a.PostAccountAsync(It.IsAny<Account>())).ReturnsAsync(new SaveAccountResponse(accountToTestSave));
+
+            var mapperProfile = new ResourceToModelProfile();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(mapperProfile));
+            _mapper = new Mapper(configuration);
+
+            
+            _accountController = new AccountController(_accountServiceMock.Object, _mapper);
         }
 
-       
+
+        #region GET TESTS
         [Test]
         public void GetAccountsForCustomerAsync_ExistingIdPassed_ReturnsCorrectDataType()
         {
@@ -57,5 +90,50 @@ namespace CustomerAPI_Tests
             //Assert
             Assert.AreEqual(validAccounts, accounts);
         }
+        #endregion
+
+        #region POST TESTS
+
+        [Test]
+        public async Task PostAccountAsync_InvalidObjectPassed_ReturnsBadRequest()
+        {
+            // Arrange
+            var nameMissingItem = new SaveAccountResource()
+            {
+                InitialCredit = 23
+            };
+            _accountController.ModelState.AddModelError("ClientID", "Required");
+
+            // Act
+            var badResponse = await _accountController.PostAccountAsync(nameMissingItem);
+
+            // Assert
+            Assert.IsInstanceOf(typeof(BadRequestObjectResult), badResponse);
+        }
+
+        [Test]
+        public async Task PostAccountAsync_ValidObjectPassed_ReturnsCreatedResponse()
+        {
+            // Act
+            var createdResponse = await _accountController.PostAccountAsync(saveAccountResource);
+            
+            // Assert
+            Assert.IsInstanceOf(typeof(OkObjectResult), createdResponse);
+        }
+
+        [Test]
+        public async Task PostAccountAsync_ValidObjectPassed_ReturnedResponseHasCreatedItem()
+        {
+            // Act
+            var createdResponse = (OkObjectResult) await _accountController.PostAccountAsync(saveAccountResource);
+            var itemAsObject = createdResponse.Value;
+            var item = (Account) itemAsObject; 
+
+            // Assert
+            Assert.IsInstanceOf(typeof(Account), itemAsObject);
+            Assert.AreEqual(accountToTestSave.Balance, item.Balance);
+        }
+
+        #endregion
     }
 }
